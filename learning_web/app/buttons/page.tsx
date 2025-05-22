3e1cb5e6121a3b4259fd41e8ce3914597509f330
Nguyentuananh <ngtanh2112@gmail.com>
2025-05-22 15:55:18 +0700

    Updated Button Libary

--- learning_web/app/buttons/page.tsx
+++ learning_web/app/buttons/page.tsx
@@ -1,9 +1,55 @@
+import { Button } from "@/components/ui/button";
+
+
+
 const ButtonPage = () => {
   return (
-    <div>
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
       
-        Click  day nay
-      
+        <Button>
+          Default
+        </Button>
+        <Button variant="primary">
+          Primary
+        </Button>
+        <Button variant="primaryOutline">
+          Primary Outline
+        </Button>
+
+        <Button variant="secondary">
+          Secondary
+        </Button>
+        <Button variant="secondaryOutline">
+          Secondary Outline
+        </Button>
+
+        <Button variant="danger">
+          Secondary
+        </Button>
+        <Button variant="dangerOutline">
+          Secondary Outline
+        </Button>
+
+        <Button variant="super">
+          Secondary
+        </Button>
+        <Button variant="superOutline">
+          Secondary Outline
+        </Button>
+
+        <Button variant="ghost">
+          ghost
+        </Button>
+        
+
+        <Button variant="sidebar">
+          sidebar
+        </Button>
+        <Button variant="sidebarOutline">
+          Secondary Outline
+        </Button>
+
+        
     </div>
   );
 };
