3d9a3ff71fc214323e3ef4d5d24b9926e62b488e
Do Minh Quan <lop11b1hahoiquando@gmail.com>
2025-06-02 14:21:04 +0700

    Up code lên nhánh version-dev

--- learning_web/app/db/schema.ts
+++ learning_web/app/db/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+export const Course = pgTable("Course", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(), 
